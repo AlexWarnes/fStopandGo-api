92d0436384cf3700d658fb39f46f3576318367cd
Alex Warnes <alex.s.warnes@gmail.com>
2018-09-09 11:21:09 -0400

    added env exports and expirations for JWT

--- config.js
+++ config.js
@@ -2,3 +2,5 @@ exports.PORT = process.env.PORT || 8080;
 exports.CLIENT_ORIGIN = 'https://fstopandgo.herokuapp.com';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/fstopandgo';
 exports.TESTING_DATABASE_URL = process.env.TESTING_DATABASE_URL || 'mongodb://localhost/testingDB';
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
